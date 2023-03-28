f98024cfae558984c4afd61d88234750ff900117
Henrique Galieta <hgalieta@gmail.com>
2023-03-28 11:01:42 -0300

    feat: add route to home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
+import { HomeComponent } from './components/home/home.component';
 import { EncomendasComponent } from './components/encomendas/encomendas.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
   {
     path: 'encomendas',
     component: EncomendasComponent,
