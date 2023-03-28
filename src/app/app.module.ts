19d61e83df30184dd44973a04c7b119993ec13da
Henrique Galieta <hgalieta@gmail.com>
2023-03-28 10:52:47 -0300

    feat: add encomendas component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { EncomendasComponent } from './components/encomendas/encomendas.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    EncomendasComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
