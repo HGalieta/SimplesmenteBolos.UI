1986833d86ad48583f5b77a3728837d6e2d8cb50
Henrique Galieta <hgalieta@gmail.com>
2023-04-03 21:50:42 -0300

    feat: add functions to list and add products

--- src/app/components/encomendas/encomendas.component.ts
+++ src/app/components/encomendas/encomendas.component.ts
@@ -1,15 +1,32 @@
+import { PedidoService } from './pedido.service';
 import { Component, OnInit } from '@angular/core';
-
+import { OrderItem } from './itemPedido';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-encomendas',
   templateUrl: './encomendas.component.html',
-  styleUrls: ['./encomendas.component.css']
+  styleUrls: ['./encomendas.component.css'],
 })
 export class EncomendasComponent implements OnInit {
+  orderItem: OrderItem = {
+    tipo: 'Caseiro',
+    sabor: 'Laranja',
+    tamanho: 20,
+  };
 
-  constructor() { }
+  order: OrderItem[] = [];
+
+  constructor(private service: PedidoService, private router: Router) {}
 
   ngOnInit(): void {
+    this.service.listOrder().subscribe((order) => {
+      this.order = order;
+    });
   }
 
+  addProduct() {
+    this.service.addItemToOrder(this.orderItem).subscribe(() => {
+      this.router.navigate(['/encomendas']);
+    });
+  }
 }
